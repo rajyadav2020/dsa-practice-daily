1d1c569773793eb992b117337972e1849f04736b
rajyadav2020 <rajyadavv176@gmail.com>
2026-02-07 14:22:22 +0530

    Search in rotated array leetcode

---
+++ Search/Search_in_rotated_aray.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+      int search(vector<int>& nums, int target) {
+          int n = nums.size();
+          int start = 0;
+          int end = n-1;
+          while(start<=end){
+              int mid = (start+end)/2;
+  
+              if(nums[mid]==target){
+                  //element found,return the index;
+                  return mid;
+  
+              }
+              else if(nums[start]<=nums[mid]){
+                  if(target>=nums[start] && target <= nums[mid]){
+                      end = mid-1;
+                  }
+                  else
+                  {
+                      start = mid+1;
+                  }
+              }
+              else
+              {
+                  if(target >= nums[mid] && target<=nums[end]){
+                      start = mid+1;
+  
+                  }
+                  else{
+                      end = mid-1;
+                  }
+              }
+          }
+          return -1;
+      }
+  };
