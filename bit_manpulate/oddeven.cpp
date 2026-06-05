53fd41630c61e414380ad555c8ee8d8a53cb6afd
rajyadav2020 <rajyadavv176@gmail.com>
2026-06-05 14:39:43 +0530

    odd or even

---
+++ bit_manpulate/oddeven.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool isEven(int n) {
+        // code here
+        if( (n & 1)==0 )
+        {
+            return true;
+        }
+        else
+        {
+            return false; 
+        }
+    }
+};
