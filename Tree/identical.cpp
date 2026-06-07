cd8f66645874255ea84a410ccccdbd035ec4406d
rajyadav2020 <rajyadavv176@gmail.com>
2026-06-07 21:23:01 +0530

    identical trees leetcode 100

---
+++ Tree/identical.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  public:
+      bool isSameTree(TreeNode* p, TreeNode* q) {
+          if(root == nullptr)
+          {
+              return true;
+          }
+  
+          return ismirror(root->left,root->right);
+      }
+  
+  private:
+      bool ismirror()
+  };
