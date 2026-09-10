3ff9b1180f696f40284f73cfb41d1dec6cd1d2f4
rajyadav2020 <rajyadavv176@gmail.com>
2026-09-10 21:50:52 +0530

    daily temperatures

---
+++ stack/dailytemp.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+      vector<int> dailyTemperatures(vector<int>& temperatures) {
+      int n = temperatures.size();
+      stack<int> s;
+      vector<int> ans(n, 0);
+  
+      for(int i = 0; i < n; i++) {
+  
+      while(!s.empty() && temperatures[s.top()] < temperatures[i]) {
+  
+          ans[s.top()] = i - s.top();
+  
+          s.pop();
+      }
+  
+      s.push(i);
+      }
+  
+      return ans;
+      }
+  };
